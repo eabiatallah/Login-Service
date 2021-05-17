4427bda3cbbffe931b7a02f45c9fb768df010599
= <=>
2021-05-16 23:30:03 -0400

    Second Commit

--- Login-registration/src/test/java/com/eaa/login/appuser/AppUserServiceTest.java
+++ Login-registration/src/test/java/com/eaa/login/appuser/AppUserServiceTest.java
@@ -20,7 +20,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import com.eaa.login.registration.token.ConfirmationToken;
 import com.eaa.login.registration.token.ConfirmationTokenService;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringRunner.class) 
 @SpringBootTest
 public class AppUserServiceTest {
 
